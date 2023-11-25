efe989a2f648967e6b9ad4e71cc04a4d4189e72e
zschreier <zjschreier@gmail.com>
2023-11-25 14:23:44 -0600

    inital try for testing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 
 const express = require("express")
+const fs = require("fs")
 const app = express()
 
 const PORT = process.env.PORT || 3001;
@@ -16,6 +17,15 @@ app.get('/', (req, res) =>
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.get('/api/notes', (req, res) => 
+  fs.readFile('db.json', 'utf8', function(err, data){
+    if(err){
+      console.log(err)
+    }
+    res.json(JSON.parse(data))
+  })
 )
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
